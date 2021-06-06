6c003195de60047a0c6a6f4f301fb9db31b48bf2
NigelO <54878113+Lord-14@users.noreply.github.com>
2021-06-06 10:09:48 -0400

    Update main.py
    Fixed issues

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from tkinter import*
+class WeatherApp:
+  def __init__(self, root):
+    self.root = root
+    self.var_search=StringVar()
+    self.root.title("Global Weather App")
+    self.root.geometry("600x400+450+100")
+    self.root.config(bg="white")
+    name=Label(self.root,text="WeatherApp", font=("agency fb", 18, "underline"))
+    name.place(x=0,y=0, relwidth=1,height=40)
+    city=Label(self.root,text="City:", font=("agency fb", 16), bg="white")
+    city.place(x=-100,y=60,relwidth=1, height=30)
+    cityEntry=Entry(self.root,textvariable=self.var_search, font=("agency fb", 15), bg="white")
+    cityEntry.place(x=230, y=60, relwidth=0.5,height = 30)
 
-def __init__(self, root):
-  self.root = root
-  self.var_search=StringVar()
-  self.root.title("Global Weather App")
-  self.root.geometry("400x400")
-  name=Label(self.root,text="WeatherApp", font=("agency fb", 18, "underline"))
-  city=Label(self.root,text="City", font=("agency fb", 16))
-  dataEntry=Entry(self.root, textvariable=self.var_search, font=("agency fb", 12)
-  self.root.config(bg="blue")
+    
   
+root=Tk() 
+obj=WeatherApp(root)
+root.mainloop()
