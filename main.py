1beac2031a873e4c9cd29dfe6e4660ec75e980ae
NigelO <54878113+Lord-14@users.noreply.github.com>
2021-06-07 10:38:16 -0400

    Update main.py
    added api key variable and url variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import*
+import aKey
 class WeatherApp:
   def __init__(self, root):
     self.root = root
@@ -12,6 +13,10 @@ class WeatherApp:
     city.place(x=-100,y=60,relwidth=1, height=30)
     cityEntry=Entry(self.root,textvariable=self.var_search, font=("agency fb", 15), bg="white", fg="green")
     cityEntry.place(x=225, y=60, relwidth=0.5,height = 30)
+  
+  def getData(self):
+    api_key= aKey.apiKey
+    url="https://api.openweathermap.org/data/2.5/weather?q={self.var_search.get()}&appid={api_key}"
 
     
   
